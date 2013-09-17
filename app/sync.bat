dc84fc36c41c5fa36fec27a3b0c69a127f035ea7
U80789367 <U80789367@38a3ecd8-5b25-7645-a312-2ec681589999>
2013-09-17 12:59:32 +0000

    sync.bat angepasst
    git-svn-id: svn://localhost/DI-Extractor@51 38a3ecd8-5b25-7645-a312-2ec681589999

--- app/sync.bat
+++ app/sync.bat
@@ -3,6 +3,6 @@ SETLOCAL
 
 SET WORKDIR=C:\Software\xampp\htdocs\isadg\app
 
-robocopy.exe %WORKDIR% P:\KOST\Tools\DI-Extractor\_workbench\xIsadg\app /MIR
+robocopy.exe %WORKDIR% P:\KOST\Tools\DI-Extractor\_workbench\app /MIR
 
 PAUSE
