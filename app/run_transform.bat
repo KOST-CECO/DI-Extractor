6e7dde2102b14c085585cdb195954273682bcca8
mkaiser56 <m.kaiser@access.ch>
2017-06-14 14:36:49 +0200

    Testprogramm run_transform.bat

--- app/run_transform.bat
+++ app/run_transform.bat
@@ -1,7 +1,48 @@
 @ECHO OFF
 SETLOCAL
 
+ECHO ===========================================================================
+ECHO Signaturstil: fortlaufend SIG.1 SIG.2 / Ausgabeformat: xIadg
+SET FONDTITLE=xIsadg-Bestand-Titel
+SET SIGNATUR=sig
+REM Signaturstil (fortlaufend SIG.1 SIG.2 / hierarchisch SIG.1 SIG.1.1): [1] oder [2]
+SET STIL=1
+REM Ausgabeformat (xIadg / EAD): [1] oder [2]
+SET FMT=1
 CALL transform.bat "../sample/SIP_20070923_arelda_v4"      "xIsadg_metadata.xml"
 CALL "C:\Tools\Altova\XMLSpy2005\XMLSpy2005.exe"           "%CD%\xIsadg_metadata.xml"
+
+ECHO ===========================================================================
+ECHO Signaturstil: hierarchisch SIG.1 SIG.1.1 / Ausgabeformat: xIadg
+SET FONDTITLE=xIsadg-Bestand-Titel
+SET SIGNATUR=sig
+REM Signaturstil (fortlaufend SIG.1 SIG.2 / hierarchisch SIG.1 SIG.1.1): [1] oder [2]
+SET STIL=2
+REM Ausgabeformat (xIadg / EAD): [1] oder [2]
+SET FMT=1
+CALL transform.bat "../sample/SIP_20070923_arelda_v4"      "xIsadg_metadata.xml"
+CALL "C:\Tools\Altova\XMLSpy2005\XMLSpy2005.exe"           "%CD%\xIsadg_metadata.xml"
+
+ECHO ===========================================================================
+ECHO Signaturstil: fortlaufend SIG.1 SIG.2 / Ausgabeformat: EAD
+SET FONDTITLE=EAD-Bestand-Titel
+SET SIGNATUR=sig
+REM Signaturstil (fortlaufend SIG.1 SIG.2 / hierarchisch SIG.1 SIG.1.1): [1] oder [2]
+SET STIL=1
+REM Ausgabeformat (xIadg / EAD): [1] oder [2]
+SET FMT=2
+CALL transform.bat "../sample/SIP_20070923_arelda_v4"      "EAD_metadata.xml"
+CALL "C:\Tools\Altova\XMLSpy2005\XMLSpy2005.exe"           "%CD%\EAD_metadata.xml"
+
+ECHO ===========================================================================
+ECHO Signaturstil: hierarchisch SIG.1 SIG.1.1 / Ausgabeformat: EAD
+SET FONDTITLE=EAD-Bestand-Titel
+SET SIGNATUR=sig
+REM Signaturstil (fortlaufend SIG.1 SIG.2 / hierarchisch SIG.1 SIG.1.1): [1] oder [2]
+SET STIL=2
+REM Ausgabeformat (xIadg / EAD): [1] oder [2]
+SET FMT=2
+CALL transform.bat "../sample/SIP_20070923_arelda_v4"      "EAD_metadata.xml"
+CALL "C:\Tools\Altova\XMLSpy2005\XMLSpy2005.exe"           "%CD%\EAD_metadata.xml"
 
 EXIT /B
--- app/transform.bat
+++ app/transform.bat
@@ -6,31 +6,31 @@ SET SAXON=C:\Tools\saxon9
 SET LINT=C:\Tools\PCUnixUtils
 
 IF [%1]==[] (
-        ECHO usage: %0  path\SIP...  OUTPUT
-        EXIT /B
+    ECHO usage: %0  path\SIP...  OUTPUT
+    EXIT /B
 )
 
 IF [%2]==[] (
-        ECHO usage: %0  path\SIP...  OUTPUT
-        EXIT /B
+    ECHO usage: %0  path\SIP...  OUTPUT
+    EXIT /B
 )
 
 IF NOT EXIST %1 (
-        ECHO SIP path %1 not found
-        ECHO usage: %0 path\SIP...
-        EXIT /B
+    ECHO SIP path %1 not found
+    ECHO usage: %0 path\SIP...
+    EXIT /B
 )
 
 IF NOT EXIST %1\header\metadata.xml (
-        ECHO %1\header\metadata.xml not found
-        ECHO usage: %0 path\SIP...
-        EXIT /B
+    ECHO %1\header\metadata.xml not found
+    ECHO usage: %0 path\SIP...
+    EXIT /B
 )
 
 SET ECH-0160=%1
 SET OUTPUT=%2
 IF EXIST %OUTPUT% (
-        DEL /Q %OUTPUT%
+    DEL /Q %OUTPUT%
 )
 
 REM User input -----------------------------------------------------------------
@@ -38,30 +38,38 @@ REM User input -----------------------------------------------------------------
 ECHO.
 ECHO Benutzereingabe
 ECHO ===============
-SET /P "FONDTITLE=Bestand Titel: "
+IF [%FONDTITLE%]==[] (
+    SET /P "FONDTITLE=Bestand Titel: "
+)
 REM left trim xslt parameter
 for /f "tokens=* delims= " %%a in ("%FONDTITLE%") do set FONDTITLE=%%a
 
-SET /P "SIGNATUR=Archivkuerzel und Bestandessignatur: "
+IF [%SIGNATUR%]==[] (
+    SET /P "SIGNATUR=Archivkuerzel und Bestandessignatur: "
+)
 REM left trim xslt parameter
 for /f "tokens=* delims= " %%a in ("%SIGNATUR%") do set SIGNATUR=%%a
 
-SET STIL=1
-SET /P "STIL=Signaturstil (fortlaufend SIG.1 SIG.2 / hierarchisch SIG.1 SIG.1.1): [1] oder [2] "
-SET REF=numberRef.xml
-IF %STIL% == 2 (
-        SET REF=null.xml
+IF [%STIL%]==[] (
+    SET STIL=1
+    SET /P "STIL=Signaturstil (fortlaufend SIG.1 SIG.2 / hierarchisch SIG.1 SIG.1.1): [1] oder [2] "
 )
 
-SET FMT=1
-SET /P "FMT=Ausgabeformat (xIadg / EAD): [1] oder [2] "
+IF [%FMT%]==[] (
+    SET FMT=1
+    SET /P "FMT=Ausgabeformat (xIadg / EAD): [1] oder [2] "
+)
 ECHO.
 
 REM create unique reference for each archival object
+SET REF=numberRef.xml
 %JAVA_HOME%\bin\java -jar %SAXON%\saxon9.jar -versionmsg:off -s:%ECH-0160%\header\metadata.xml -xsl:createRef.xsl -o:"createRef.xml" fondtitle=%FONDTITLE% archsig=%SIGNATUR%
 
 REM create running number for each archival object
 %JAVA_HOME%\bin\java -jar %SAXON%\saxon9.jar -versionmsg:off -s:createRef.xml -xsl:elementRef.xsl -o:%REF% fondtitle=%FONDTITLE% archsig=%SIGNATUR%
+IF %STIL% == 2 (
+    SET REF=null.xml
+)
 
 REM convert to xIsadg and validate with xmllint
 IF %FMT% == 1 (
@@ -79,7 +87,7 @@ REM schema validate with xmllint
 
 ECHO.
 IF %ERRORLEVEL%==0 (
-        ECHO SIP %ECH-0160% converted
-        ECHO output is %OUTPUT%
-        ECHO.
+    ECHO SIP %ECH-0160% converted
+    ECHO output is %OUTPUT%
+    ECHO.
 )
