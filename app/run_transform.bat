2037770cab6a797f5e062ae5e4c9a5669d270823
mkaiser56 <m.kaiser@access.ch>
2017-10-02 17:46:17 +0200

    aufräumen

--- app/run_transform.bat
+++ app/run_transform.bat
@@ -69,4 +69,53 @@ SET FMT=3
 CALL transform.bat "../sample/SIP_20070923_arelda_v4"      "EAD_metadata.xml"
 CALL "C:\Tools\Altova\XMLSpy2005\XMLSpy2005.exe"           "%CD%\EAD_metadata.xml"
 
+ECHO ===========================================================================
+SET INPUT=arelda_v4-vollstaendig-GEVER.xml
+ECHO Signaturstil: fortlaufend SIG.1 SIG.2 / Ausgabeformat: xIadg 2.0
+SET FONDTITLE=Bestand-Titel
+SET SIGNATUR=sig
+REM Signaturstil (fortlaufend SIG.1 SIG.2 / hierarchisch SIG.1 SIG.1.1): [1] oder [2]
+SET STIL=1
+REM Ausgabeformat (xIadg / xIsadg 2.0 / EAD ): [1] [2] oder [3]
+SET FMT=2
+CALL transform.bat "../sample/%INPUT%"                 "xIsadg.2.0_metadata.xml"
+IF %ERRORLEVEL% NEQ 0 (
+   PAUSE
+   EXIT /B
+)
+CALL "C:\Tools\Altova\XMLSpy2005\XMLSpy2005.exe"  "%CD%\xIsadg.2.0_metadata.xml"
+
+
+ECHO ===========================================================================
+SET INPUT=arelda_v4.1-vollstaendig-GEVER.xml
+ECHO Signaturstil: fortlaufend SIG.1 SIG.2 / Ausgabeformat: xIadg 2.0
+SET FONDTITLE=Bestand-Titel
+SET SIGNATUR=sig
+REM Signaturstil (fortlaufend SIG.1 SIG.2 / hierarchisch SIG.1 SIG.1.1): [1] oder [2]
+SET STIL=1
+REM Ausgabeformat (xIadg / xIsadg 2.0 / EAD ): [1] [2] oder [3]
+SET FMT=2
+CALL transform.bat "../sample/%INPUT%"                 "xIsadg.2.0_metadata.xml"
+IF %ERRORLEVEL% NEQ 0 (
+   PAUSE
+   EXIT /B
+)
+CALL "C:\Tools\Altova\XMLSpy2005\XMLSpy2005.exe"  "%CD%\xIsadg.2.0_metadata.xml"
+
+ECHO ===========================================================================
+SET INPUT=arelda_v4-vollstaendig-FILES.xml
+ECHO Signaturstil: fortlaufend SIG.1 SIG.2 / Ausgabeformat: xIadg 2.0
+SET FONDTITLE=Bestand-Titel
+SET SIGNATUR=sig
+REM Signaturstil (fortlaufend SIG.1 SIG.2 / hierarchisch SIG.1 SIG.1.1): [1] oder [2]
+SET STIL=1
+REM Ausgabeformat (xIadg / xIsadg 2.0 / EAD ): [1] [2] oder [3]
+SET FMT=2
+CALL transform.bat "../sample/%INPUT%"                 "xIsadg.2.0_metadata.xml"
+IF %ERRORLEVEL% NEQ 0 (
+   PAUSE
+   EXIT /B
+)
+CALL "C:\Tools\Altova\XMLSpy2005\XMLSpy2005.exe"  "%CD%\xIsadg.2.0_metadata.xml"
+
 EXIT /B
