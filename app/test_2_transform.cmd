df1e100a039280e1c5cc51c470e8147a88fbed65
mkaiser56 <m.kaiser@access.ch>
2018-02-12 15:48:22 +0100

    update win 10 migration

---
+++ app/test_2_transform.cmd
@@ -0,0 +1,47 @@
+@ECHO OFF 
+SETLOCAL
+
+SET INPUT=arelda_v4-minimal-GEVER.xml
+
+ECHO ===========================================================================
+ECHO Signaturstil: fortlaufend SIG.1 SIG.2 / Ausgabeformat: xIadg 2.0
+SET FONDTITLE=Bestand-Titel
+SET SIGNATUR=sig
+REM Signaturstil (fortlaufend SIG.1 SIG.2 / hierarchisch SIG.1 SIG.1.1): [1] oder [2]
+SET STIL=1
+REM Ausgabeformat (xIadg / xIsadg 2.0 / EAD ): [1] [2] oder [3]
+SET FMT=2
+CALL transform.cmd "../sample/%INPUT%"      "xIsadg.2.0_metadata_min.xml"
+
+IF %ERRORLEVEL% NEQ 0 (
+   PAUSE
+   EXIT /B
+)
+
+REM PAUSE
+
+CALL "C:\Tools\Altova\XMLSpy2005\XMLSpy2005.exe"                   "%CD%\xIsadg.2.0_metadata_min.xml"
+
+
+SET INPUT=arelda_v4.1-vollstaendig-GEVER.xml
+
+ECHO ===========================================================================
+ECHO Signaturstil: fortlaufend SIG.1 SIG.2 / Ausgabeformat: xIadg 2.0
+SET FONDTITLE=Bestand-Titel
+SET SIGNATUR=sig
+REM Signaturstil (fortlaufend SIG.1 SIG.2 / hierarchisch SIG.1 SIG.1.1): [1] oder [2]
+SET STIL=1
+REM Ausgabeformat (xIadg / xIsadg 2.0 / EAD ): [1] [2] oder [3]
+SET FMT=2
+CALL transform.cmd "../sample/%INPUT%"      "xIsadg.2.0_metadata.xml"
+
+IF %ERRORLEVEL% NEQ 0 (
+   PAUSE
+   EXIT /B
+)
+
+REM PAUSE
+
+CALL "C:\Tools\Altova\XMLSpy2005\XMLSpy2005.exe"                   "%CD%\xIsadg.2.0_metadata.xml"
+
+EXIT /B
