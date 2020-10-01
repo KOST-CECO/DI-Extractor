580fdc6bf6b93f81b8fdaff4a498676a1822c172
mkaiser56 <m.kaiser@access.ch>
2020-10-01 16:23:56 +0200

    Copyright Zeile und Links korrigiert

--- app/copyright.php
+++ app/copyright.php
@@ -1,17 +1,6 @@
 <?php
 if ($wdir=='') { header ("location: ./input.php"); }
 ?>
-<hr>
-<table style="width: 100%;">
-	<tr>
-		<td style="text-align: left;">
-			<span class="copyright">
-				Copyright &copy; 2005-2020 by Koordinationsstelle f&uuml;r die dauerhafte Archivierung elektronischer Unterlagen
-			</span></td>
-		<td align="right">
-			<a href="mailto:info@kost-ceco.ch">
-				<span class="copyright">
-					info@kost-ceco.ch
-				</span></a></td>
-	</tr>
-</table>
+<span class="copyright">
+	<a href="https://kost-ceco.ch/">Copyright © 2005-2020 by Koordinationsstelle für die dauerhafte Archivierung elektronischer Unterlagen</a>
+</span>
--- app/helptext.php
+++ app/helptext.php
@@ -7,16 +7,16 @@ if ($wdir=='') { header ("location: ./input.php"); }
 			konvertiert werden können aber nur eCH-0160 Metadaten. Ziel ist das Extrahieren der ISAD(G) relevanten Metadaten, die
 			<i>Descriptive Information</i> nach OAIS für den Import in ein
 			<i>Datamanagement</i> oder ein Findmittel und nicht aber die vollständige Abbildung aller eCH-0160 Metadaten in einem anderen Schema. Grundlage der Konvertierung ist das Papier
-			<a href="https://www.ech.ch/alfresco/s/ech/download?nodeid=e05d969b-1bff-4593-ad57-d751cc5473aa">Extraktion deskriptiver Metadaten aus der archivischen Ablieferungsschnittstelle eCH-0160</a> und der Crosswalk
-			<a href="https://kost-ceco.ch/isadg/app_beta/eCH-0160_xIsadg&amp;EAD_v2.1.xlsx">eCH-0160_xIsadg&amp;EAD_v2.1.xlsx</a>. Unterstützt werden die Ausgangsformate eCH-0160 1.0 und eCH-0160 1.1; Zielformat ist xIsadg v1.6.1 und xIsadg v2.1, EAD in der Ausprägung METS Matterhorn Profil und RIC-O Version 0.1
+			<a href="https://test.ech.ch/alfresco/s/ech/download?nodeid=e05d969b-1bff-4593-ad57-d751cc5473aa">Extraktion deskriptiver Metadaten aus der archivischen Ablieferungsschnittstelle eCH-0160</a> und der Crosswalk
+			<a href="https://kost-ceco.ch/cms/dl/91141f46022f5f2dd1f8fa9409d2dcab/eCH-0160_xIsadg%26EAD_v2.1.xlsx">eCH-0160_xIsadg&amp;EAD_v2.1.xlsx</a>. Unterstützt werden die Ausgangsformate eCH-0160 1.0 und eCH-0160 1.1; Zielformat ist xIsadg v1.6.1 und xIsadg v2.1, EAD in der Ausprägung METS Matterhorn Profil und RIC-O Version 0.1
 			<br> Technisch basiert die Konvertierung auf folgenden XSLT Transformationen
-			<a href="https://kost-ceco.ch/isadg/app_beta/eCH-0160_xIsadg&amp;EAD&amp;RIC.xsl.zip">eCH-0160_xIsadg&amp;EAD&amp;RIC.xsl.zip</a>
+			<a href="./eCH-0160_xIsadg&amp;EAD&amp;RIC.xsl.zip">eCH-0160_xIsadg&amp;EAD&amp;RIC.xsl.zip</a>
 			<br> Eine Windows Commandline Batch Datei mit Saxon9 und xmllint ist in dieser ZIP Datei ebenfalls zu finden.
 			<br> Die Basisdokumente für die Metadatenschema sind die folgenden
 			<i>Data Dictionaries</i> :
 			<br>
-			<a href="https://www.ech.ch/alfresco/s/ech/download?nodeid=4bf53ff1-0e4f-4fb9-9ccc-75a4219bb20c">eCH-0160_V1.0_ArchivischeAblieferungsschnittstelle_DataDictionary.pdf</a> und
-			<a href="https://kost-ceco.ch/isadg/app_beta/xIsadg_DataDictionary_v2.1.pdf">xIsadg_DataDictionary_v2.1.pdf</a>
+			<a href="http://ech.ch/de/dokument/4bf53ff1-0e4f-4fb9-9ccc-75a4219bb20c">eCH-0160_V1.1_ArchivischeAblieferungsschnittstelle_DataDictionary.pdf</a> und
+			<a href="https://kost-ceco.ch/cms/dl/b890ad3a2222023b4247d17779062619/xIsadg_DataDictionary_v2.1.pdf">xIsadg_DataDictionary_v2.1.pdf</a>
 			<br>
 			<br>
 		</p>
